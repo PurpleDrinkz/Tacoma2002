657cdd82f310f50671d8df331f6c8e59e5eddcc5
Benjamin <alumno@E121>
2018-05-09 11:10:44 -0700

    Tacoma viejita perrona.

--- Graficos/Graficos.cpp
+++ Graficos/Graficos.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define GLEW_STATIC
-#include "GL\glew.h"
+#include "GL\glew.h"  
 #include "GLFW\glfw3.h"
 #include "glm\gtx\transform.hpp"
 #include "glm\glm.hpp"
